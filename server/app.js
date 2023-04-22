2d9ed8d869921fc07382e9f4ae2333bff1bb66ee
Aleksandr7739 <115591062+Aleksandr7739@users.noreply.github.com>
2023-04-22 09:33:01 +0300

    Revert "Добавил cors и bodyParser"

--- server/app.js
+++ server/app.js
@@ -1,26 +1,5 @@
-const cors = require('cors');
 const express = require('express')
-var bodyParser = require('body-parser')
 const app = express()
-
-app.use(cors({
-    origin: '*'
-}))
-
-const config = {
-    user: 'postgres',
-    database: 'travel-database',
-    password: 'postgres',
-    port: 5432
-};
-
-const pool = new pg.Pool(config);
-
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
@@ -28,24 +7,19 @@ app.get('/', function (req, res) {
 
 app.get('/travel-city', function (req, res) {
     console.log("Запрос получен");
-
-    pool.connect(function (err, client, done) {
-        console.log('Проверка бэка номер 1')
-        if (err) {
-            console.log("Can not connect to the DB" + err);
+    // Загрузка списка городов из базы
+    let resultCitys = [
+        {
+            nameCity: "Москва"
+        },
+        {
+            nameCity: "Питер"
+        },
+        {
+            nameCity: "Сочи"
         }
-        client.query('SELECT * FROM city order by id', function (err, result) {
-            done();
-            if (err) {
-                console.log(err);
-                return res.status(400).send(err);
-            }
-            // console.log(result.rows)
-            console.log('Проверка бэка номер 2')
-            return res.status(200).json({ response: result.rows });
-        })
-    })
-    // res.json(resultCitys)
+    ]
+    res.json(resultCitys)
 })
 
 app.listen(5000, console.log("Server work"))
