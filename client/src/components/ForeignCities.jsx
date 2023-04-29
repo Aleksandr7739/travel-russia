4f0ec25486918f3e3a35a02e153ba041a4e0dfe8
igordg777 <igordg@mail.ru>
2023-04-29 11:06:59 +0300

    Удаляет лишний текст

--- client/src/components/ForeignCities.jsx
+++ client/src/components/ForeignCities.jsx
@@ -5,7 +5,7 @@ function ForeignCities() {
         <div>
             Foreign Cities
 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus et atque iusto. Tempore ad, asperiores esse eligendi veritatis quaerat quae recusandae totam nulla impedit obcaecati vel enim aut illum.
+
         </div>
     )
 }
