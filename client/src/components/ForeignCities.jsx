44ec59cf389d1dfb40d31b9c1993517a0b9f2a8e
igordg777 <igordg@mail.ru>
2023-04-29 09:53:44 +0300

    Добавляет новый компонент ForeignCities

--- client/src/components/ForeignCities.jsx
+++ client/src/components/ForeignCities.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function ForeignCities() {
     return (
         <div>
-            Foreign Cities!!!
+            Foreign Cities
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus et atque iusto. Tempore ad, asperiores esse eligendi veritatis quaerat quae recusandae totam nulla impedit obcaecati vel enim aut illum.
         </div>
     )
 }
